2c82dbd304410482a845c920043b5429037085ee
takumiya081 <takumiya081@gmail.com>
2024-07-01 20:03:38 +0900

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
 
 import {Button} from '@mantine/core';
+import type {MouseEventHandler} from 'react';
 
 import img from '../../public/image.jpg';
 
 const IndexPage = (): JSX.Element => {
-  console.log('test');
+  const ref = React.useRef<HTMLInputElement>(null);
+
+  const handleClickFileInput: MouseEventHandler<HTMLInputElement> = (e) => {
+    e.currentTarget.value = '';
+  };
+
   return (
     <>
-      this is index<Button>test</Button>
+      <Button
+        onClick={() => {
+          ref.current?.click();
+        }}
+      >
+        test
+      </Button>
+      <input
+        ref={ref}
+        accept="image/png,image/jpg,image/jpeg,image/gif"
+        type="file"
+        onChange={(e) => {
+          console.log(e);
+        }}
+        onClick={handleClickFileInput}
+      />
       <div>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img alt="" src={img.src} style={{width: '100%', height: 'auto'}} />
